78b5a5a7dc9adb1f8d00e301c3ec6854beb2198f
lavangais <lavangais37@gmail.com>
2024-03-28 14:53:28 +0100

    Set counter and choice system

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function startTimer(duration, display) {
             blip2.play();
             timer = 0;
             stopTimer(duration);
+            set_count_up();
         }
         if (timer < 5) {
             blip.play();
@@ -37,11 +38,12 @@ function stopTimer(duration) {
     display.textContent = minutes + ":" + seconds;
 
     state = "start";
-    start_stop.textContent = "Start"
+    start_stop.textContent = "Start";
+    start_stop.style.backgroundColor = "chartreuse";
 }
 
 function start_stop_timer() {
-    time = parseInt(timer_input.value); // your time in seconds here
+    time = parseInt(restTime); 
     if (state == "start") {
         startTimer(time, display);
         state = "stop";
@@ -49,23 +51,49 @@ function start_stop_timer() {
         start_stop.style.backgroundColor = "red";
     } else if (state == "stop") {
         stopTimer(time);
-        state = "start"
-        start_stop.textContent = "Start"
+        state = "start";
+        start_stop.textContent = "Start";
         start_stop.style.backgroundColor = "chartreuse";
     }
 }
 
-start_stop = document.querySelector("button");
+function set_count_up() {
+    current = document.querySelector(".current");
+    current.textContent = parseInt(current.textContent)+1;
+}
+
+start_stop = document.querySelector("button.start");
 state = "start"
 display = document.querySelector('.timer p');
-timer_input = document.querySelector(".timer input");
+ukupno_serija = document.querySelector(".ukupno_serija");
 
 start_stop.addEventListener("click", function () {
     start_stop_timer();
 });
 
-timer_input.addEventListener("keyup", function(event) {
-    if (event.key == "Enter") {
-        start_stop_timer();
-    }
+// CHOOSING PART
+choice = document.querySelector(".choice");
+timer_div = document.querySelector(".timer");
+
+
+broj_serija = document.querySelector(".choice .set");
+rest = document.querySelector(".choice .rest");
+submit = document.querySelector(".choice .submit");
+
+submit.addEventListener("click", function () {
+    restTime = parseInt(rest.value);
+    ukupno_serija.textContent = broj_serija.value;
+
+    var timer = restTime, minutes, seconds;
+    minutes = parseInt(timer / 60, 10)
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    choice.style.display = "none";
+    timer_div.style.display = "flex";
+
 });
